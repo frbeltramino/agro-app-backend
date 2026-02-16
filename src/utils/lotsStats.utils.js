30566d727ca575c682e3f38ad25ab5425f0b1e35
Federico Beltramino <frbeltra2@gmail.com>
2026-02-16 14:12:20 +0100

    fix para lote sin datos

--- src/utils/lotsStats.utils.js
+++ src/utils/lotsStats.utils.js
@@ -1,10 +1,6 @@
 export const emptyLot = (lot) => ({
   id: lot.id,
   lote: lot.name,
-  superficieHa: Number(lot.hectares),
-  insumos: 0,
-  labores: 0,
-  cosecha: 0,
-  costoVariable: 0,
-  margenBruto: 0,
+  superficieHa: Number(lot.hectares.toFixed(2)),
+  cultivos: [],
 });
